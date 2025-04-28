072405c67e4d4a2e3352f8f1f38797bb91ec112f
mannat <mannat.k.gill.511@gmail.com>
2025-04-28 14:05:17 -0700

    Updated functionality.
    Using local storage to save tasks, if page closes or reloads. added a button to remove tasks for today. updated functionality to remember tasks that were cancelled out.

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-let counter = 1; 
+
+if(localStorage.getItem("items") != null){
+    console.log(localStorage.getItem("items")); 
+    let list = document.getElementById("list")
+    list.innerHTML = localStorage.getItem("items"); 
+}else{
+    localStorage.setItem("items", ""); 
+}
+
+
+let counter; 
+
+if(localStorage.getItem("counter") != null){
+    counter = localStorage.getItem("counter"); 
+    console.log("counter: " + counter); 
+
+     //add back button functionality 
+     //list = document.querySelectorAll("li")
+    for(let x = 0 ; x < counter ; x++){
+        console.log(x);
+        let but = document.getElementById("item"+x); 
+        if(but == null){
+            console.log("button not found");
+        }else{
+            but.onclick = function() {
+                updateItemCheck(this, (this.id));
+            };
+        }
+    }
+ 
+}
+else{
+    counter = 0; 
+    localStorage.setItem("counter", 0);
+}
 
 document.addEventListener('keydown', function(event){
     if(event.key == "Enter" && document.getElementById("inputBar").value != ""){
+        console.log("counter value: ", counter)
         //create li element 
         let item = document.createElement("li"); 
 
@@ -9,10 +44,14 @@ document.addEventListener('keydown', function(event){
         let button = document.createElement("input"); 
         button.type = "checkbox"; 
         button.id = "item" + counter;
+        //need an event handler still because otherwise localStorage will not save updated info on whats been checked off 
+        button.onclick = function() {
+            updateItemCheck(this, (this.id));
+        };
+
         let label = document.createElement("label")
         label.textContent = document.getElementById("inputBar").value; 
         label.htmlFor = "item" + counter; 
-
 
         //appending checkbox and label to li item 
         item.appendChild(button); 
@@ -27,5 +66,34 @@ document.addEventListener('keydown', function(event){
         //update counter for next element 
         counter++; 
 
+        //update local storage 
+        localStorage.setItem("items", document.getElementById("list").innerHTML); 
+        localStorage.setItem("counter", counter);
+
+        
+
     }
 }); 
+
+function updateItemCheck(button, id){
+    console.log("In here"); 
+    console.log("id value: ", id); 
+    let label = document.querySelector(`label[for="${id}"]`);
+    if(label == null){
+        console.log("is nulll"); 
+    }
+    console.log(label); 
+    if(button.checked == true){
+        label.style.textDecoration = "line-through";
+    }else{
+        label.style.textDecoration = "none";
+    }
+    
+    localStorage.setItem("items", document.getElementById("list").innerHTML)
+}
+
+function clearItems(){
+    localStorage.setItem("items", ""); 
+    localStorage.setItem("counter", 0);
+    location.reload(); 
+}
